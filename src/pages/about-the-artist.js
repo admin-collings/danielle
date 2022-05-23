181e25ab8b2cc15f6b07274d5344e4c6438bdeff
dexidev <dddexadgat@gmail.com>
2022-05-23 16:06:02 +0800

    added wallet connect button

--- src/pages/about-the-artist.js
+++ src/pages/about-the-artist.js
@@ -3,6 +3,13 @@ import ReactDOM from 'react-dom';
 import { Link } from 'react-router-dom';
 
 const AboutTheArtist = () => {
+
+    const handleWalletConnect = () => {
+
+        //handle connecting wallet here
+
+    }
+
     return ( 
         <div className="container-fluid bg-white">
             <nav className="navbar navbar-expand-lg navbar-light bg-white py-4" id="navbar">
@@ -18,7 +25,9 @@ const AboutTheArtist = () => {
                                 </Link>
                             </div>
                         </div>
-                        <div className="col empty-nav-col"></div>
+                        <div className="col my-auto empty-nav-col">
+                            <button onClick={handleWalletConnect} className='btn btn-primary btn-rounded wallet__connect'>Wallet Connect</button>
+                        </div>
                     </div>
                 </div>
             </nav>
