4190647d50da73df8cb395db5464bc045f88a2fc
dexdev <dddexadgat@gmail.com>
2022-06-15 10:30:40 +0800

    removed some section

--- src/pages/item.js
+++ src/pages/item.js
@@ -19,7 +19,7 @@ const Item = () => {
         
             <AboutTheArtist />
           
-            <div className="container-fluid py-5 bg-white">
+            <div className="container-fluid  bg-white">
                 <div className="container py-5">
                     <div className="row justify-content-between">
                         <div className="col-md-5">
@@ -64,24 +64,19 @@ const Item = () => {
                             <br />
                             <hr />
                             <p className="text-blue h4">{currentItem.price} ETH</p>
-                            <button className="btn btn-primary mt-md-4 buy-now-btn text-white">Lorem ipsum</button>
+                            <button className="btn btn-primary mt-md-4 buy-now-btn text-white">Buy</button>
                         </div>
                     </div>
                 </div>
             </div>
  
-            <div className="container-fluid bg-white py-5" id="about-item-section">
-                <div className="container py-5">
+            <div className="container-fluid bg-white py-5 d-none" id="about-item-section">
+                <div className="container">
                     <div className="row">
                         <div className="col-md-8">
-                            <p className="h2 fw-bold mb-md-4">About the Work</p>
-                            <p className="mt-2">
-                                Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium voluptatum aliquid iste aliquam, nihil iure. Accusamus esse voluptatum quam velit optio qui autem aperiam libero ab ullam dolorem id, incidunt ducimus labore vitae, asperiores laudantium similique, hic ex eos error magnam quidem? Quas est error nihil rerum, consequatur placeat reiciendis earum dicta? Perferendis rem harum maiores ipsum, consequuntur accusamus, nostrum laborum maxime modi exercitationem nihil quidem voluptates? Iure atque laborum voluptatibus illo impedit harum esse. Velit nobis facilis fuga impedit quae eveniet odio ut! Illo doloremque impedit illum veniam in.
-                                <br /> <br />
-                                Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium voluptatum aliquid iste aliquam, nihil iure. Accusamus esse voluptatum quam velit optio qui autem aperiam libero ab ullam dolorem id, incidunt ducimus labore vitae, asperiores laudantium similique, hic ex eos error magnam quidem? Quas est error nihil rerum, consequatur placeat reiciendis earum dicta? Perferendis rem harum maiores ipsum, consequuntur accusamus, nostrum laborum maxime modi exercitationem nihil quidem voluptates? Iure atque laborum voluptatibus illo impedit harum esse. Velit nobis facilis fuga impedit quae eveniet odio ut! Illo doloremque impedit illum veniam in.
-                            </p>
+                            
                         </div>
-                        <div className="col-md-4 pt-5">
+                        <div className="col-md-4">
                             <div>
                                 <p className="text-black my-0 py-0 fw-bolder">Contract Address</p>
                                 <p className="text-muted">000000000000000000000</p>
