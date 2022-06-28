e04baa788eb8eba867922bf2b29e01568a94429a
dexdev <dddexadgat@gmail.com>
2022-06-28 10:11:38 +0800

    text changed in nft-collection component

--- src/sections/nft-collection.js
+++ src/sections/nft-collection.js
@@ -10,9 +10,7 @@ const NFTCollection = () => {
                 <div className="d-flex justify-content-center">
                     <div className="w-75">
                         <p className="text-center hero-description">
-                        With personalities like Nelson Mandela, Martin Luther King, Stephen Hawking among a host of other notable figures who have made an impact on world history and society, this new collection from Danielle honours those who have inspired, innovated, and shaped human potential across eras. 
-                        <br /><br />
-                        Exquisite in detail and original in concept, Danielle effectively depicts the genius of these timeless personalities as she immortalises their greatness on the blockchain, turning them into paragons—sources of contemplation of what the future generations should aspire to be and achieve.
+                        With personalities like Nelson Mandela, Oprah, Stephen Hawking among a host of other notable figures who have made an impact on world history and society, this new collection from Danielle honours those who have inspired, innovated, and shaped human potential across eras.
                         </p>
                     </div>
                 </div>
