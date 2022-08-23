ce763f0ec6894311f6a3e8f2528df6ed2ef50f2f
dexidev <dddexadgat@gmail.com>
2022-08-24 00:32:09 +0800

    Create Raffle Component

---
+++ src/components/raffle.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+
+const Raffle = () => {
+
+    return (
+        <p>
+            Each NFT purchased counts as one raffle to win the original painting.
+            If you're unsuccessful in winning the original painting, you will receive a physical A2 print of the respected piece. 
+            All purchases will receive a special memorabilia gift from Danielle too.
+        </p>
+    )
+
+}
+
+
+export default Raffle;
