5d247c703e6cad161a4faad175c768dab12e9acf
dexidev <dddexadgat@gmail.com>
2022-11-08 02:13:13 +0800

    added scroll to top

--- src/App.js
+++ src/App.js
@@ -1,21 +1,24 @@
 import Home from "./pages/home";
 import Item from "./pages/item";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import ScrollToTop from "./components/scrollToTop";
 
 function App() {
   return (
     <Router>
-      <Switch>
+      <ScrollToTop>
+        <Switch>
 
-        <Route exact path="/">
-          <Home />
-        </Route>
+          <Route exact path="/">
+            <Home />
+          </Route>
 
-        <Route path="/item/:id">
-          <Item />
-        </Route>
+          <Route path="/item/:id">
+            <Item />
+          </Route>
 
-      </Switch>
+        </Switch>
+      </ScrollToTop>
     </Router>
   );
 }
